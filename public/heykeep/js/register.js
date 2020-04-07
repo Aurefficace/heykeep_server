485d4dd49a8c29c079f960dae669fb0071774d3f
flematthias <flematthias@gmail.com>
2020-04-07 11:24:10 +0200

    change labels js register form

--- public/heykeep/js/register.js
+++ public/heykeep/js/register.js
@@ -11,6 +11,14 @@ form.children("div").steps({
     headerTag: "h3",
     bodyTag: "section",
     transitionEffect: "slideLeft",
+    labels: {
+        cancel: 'Annuler',
+        current: '',
+        finish: "C'est parti",
+        next: "Suivant",
+        previous: "Précédent",
+        loading: "Chargement ..."
+    },
     onStepChanging: function (event, currentIndex, newIndex)
     {
         form.validate().settings.ignore = ":disabled,:hidden";
