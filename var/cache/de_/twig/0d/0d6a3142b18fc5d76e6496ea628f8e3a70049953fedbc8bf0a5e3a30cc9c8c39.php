b7dd40afa6ef2232e14d2029dc96856dd76b4382
flematthias <flematthias@gmail.com>
2020-04-20 09:39:27 +0200

    add header footer

---
+++ var/cache/de_/twig/0d/0d6a3142b18fc5d76e6496ea628f8e3a70049953fedbc8bf0a5e3a30cc9c8c39.php
@@ -0,0 +1,200 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* security/resetPassword.html.twig */
+class __TwigTemplate_ffe7caf70e2fd5e82c6e76a33ef882f729913602b8f60e6bc49df7369a695731 extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->blocks = [
+            'title' => [$this, 'block_title'],
+            'stylesheets' => [$this, 'block_stylesheets'],
+            'body' => [$this, 'block_body'],
+        ];
+    }
+
+    protected function doGetParent(array $context)
+    {
+        // line 1
+        return "base.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "security/resetPassword.html.twig"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "security/resetPassword.html.twig"));
+
+        $this->parent = $this->loadTemplate("base.html.twig", "security/resetPassword.html.twig", 1);
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+    }
+
+    // line 3
+    public function block_title($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "title"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "title"));
+
+        echo "Log in!
+";
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+    }
+
+    // line 5
+    public function block_stylesheets($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "stylesheets"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "stylesheets"));
+
+        // line 6
+        echo "\t<link href=\"";
+        echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl("heykeep/css/login.css"), "html", null, true);
+        echo "\" rel=\"stylesheet\" type=\"text/css\">
+";
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+    }
+
+    // line 8
+    public function block_body($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "body"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "body"));
+
+        // line 9
+        echo "\t<div class=\"limiter\">
+\t\t<form class=\"login100-form validate-form\" method=\"post\">
+\t\t\t<div class=\"wrap-input100 validate-input\" data-validate=\"Password is required\">
+\t\t\t\t<label for=\"password\">Nouveau mot de passe *</label>
+\t\t\t\t<input type=\"password\" name=\"password\" id=\"inputPassword\" class=\"input100\" required>
+\t\t\t\t<span class=\"focus-input100\"></span>
+\t\t\t\t<span class=\"symbol-input100\">
+\t\t\t\t\t<i class=\"fa fa-lock\" aria-hidden=\"true\"></i>
+\t\t\t\t</span>
+\t\t\t</div>
+\t\t\t<div class=\"wrap-input100 validate-input\" data-validate=\"Password is required\">
+\t\t\t\t<label for=\"passwordConfirmation\">Confirmation Nouveau Mot de passe *</label>
+\t\t\t\t<input type=\"password\" name=\"passwordConfirmation\" id=\"inputPasswordConfirmation\" class=\"input100\" required>
+\t\t\t\t<span class=\"focus-input100\"></span>
+\t\t\t\t<span class=\"symbol-input100\">
+\t\t\t\t\t<i class=\"fa fa-lock\" aria-hidden=\"true\"></i>
+\t\t\t\t</span>
+\t\t\t</div>
+\t\t\t<div class=\"container-login100-form-btn\">
+\t\t\t\t<button class=\"login100-form-btn\" type=\"submit\">
+\t\t\t\t\tReset Password
+\t\t\t\t</button>
+\t\t\t</div>
+\t\t</form>
+\t</div>
+";
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "security/resetPassword.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  113 => 9,  103 => 8,  90 => 6,  80 => 5,  60 => 3,  37 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("{% extends 'base.html.twig' %}
+
+{% block title %}Log in!
+{% endblock %}
+{% block stylesheets %}
+\t<link href=\"{{ asset('heykeep/css/login.css') }}\" rel=\"stylesheet\" type=\"text/css\">
+{% endblock %}
+{% block body %}
+\t<div class=\"limiter\">
+\t\t<form class=\"login100-form validate-form\" method=\"post\">
+\t\t\t<div class=\"wrap-input100 validate-input\" data-validate=\"Password is required\">
+\t\t\t\t<label for=\"password\">Nouveau mot de passe *</label>
+\t\t\t\t<input type=\"password\" name=\"password\" id=\"inputPassword\" class=\"input100\" required>
+\t\t\t\t<span class=\"focus-input100\"></span>
+\t\t\t\t<span class=\"symbol-input100\">
+\t\t\t\t\t<i class=\"fa fa-lock\" aria-hidden=\"true\"></i>
+\t\t\t\t</span>
+\t\t\t</div>
+\t\t\t<div class=\"wrap-input100 validate-input\" data-validate=\"Password is required\">
+\t\t\t\t<label for=\"passwordConfirmation\">Confirmation Nouveau Mot de passe *</label>
+\t\t\t\t<input type=\"password\" name=\"passwordConfirmation\" id=\"inputPasswordConfirmation\" class=\"input100\" required>
+\t\t\t\t<span class=\"focus-input100\"></span>
+\t\t\t\t<span class=\"symbol-input100\">
+\t\t\t\t\t<i class=\"fa fa-lock\" aria-hidden=\"true\"></i>
+\t\t\t\t</span>
+\t\t\t</div>
+\t\t\t<div class=\"container-login100-form-btn\">
+\t\t\t\t<button class=\"login100-form-btn\" type=\"submit\">
+\t\t\t\t\tReset Password
+\t\t\t\t</button>
+\t\t\t</div>
+\t\t</form>
+\t</div>
+{% endblock %}
+", "security/resetPassword.html.twig", "D:\\workspace\\heykeep_server\\templates\\security\\resetPassword.html.twig");
+    }
+}
