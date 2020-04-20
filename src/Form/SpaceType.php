21e38bbd33cb12c17dc6f0341782bcfa004188e3
flematthias <flematthias@gmail.com>
2020-04-20 15:50:10 +0200

    add

---
+++ src/Form/SpaceType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Space;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SpaceType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class, ['label' => 'Nom'])
+            ->add('description', TextType::class, ['label' => 'Description'])
+//            ->add('categorie', TextType::class, ['label' => 'Catégorie'])
+//            ->add('image', FileType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Space::class,
+        ]);
+    }
+}
