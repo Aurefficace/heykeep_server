a1311e53e0658e81c00dd03455d3b3407a368c42
stephanie.debotton <stephanie.debotton@adenia.fr>
2020-04-21 09:36:25 +0200

    fix problème de validator de form

--- public/heykeep/js/register.js
+++ public/heykeep/js/register.js
@@ -1,50 +1,54 @@
-const form = $("#registration_form");
-form.validate({
-    errorPlacement: function errorPlacement(error, element) { element.before(error); },
-    rules: {
-        registration_form_plainPassword: {
-            minlength: 6
-        },
-        confirm: {
-            equalTo: "#registration_form_plainPassword"
+$(document).ready(function() {
+    const form = $("#registration_form");
+    form.validate({
+        errorPlacement: function errorPlacement(error, element) { element.before(error); },
+        rules: {
+            'registration_form[plainPassword]': {
+                minlength: 6
+            },
+            confirm: {
+                equalTo: "#registration_form_plainPassword"
+            }
         }
-    }
+    });
+    form.children("div").steps({
+        headerTag: "h3",
+        bodyTag: "section",
+        transitionEffect: "slideLeft",
+        enableFinishButton: true,
+        labels: {
+            cancel: 'Annuler',
+            current: '',
+            finish: "C'est parti",
+            next: "Suivant",
+            previous: "Précédent",
+            loading: "Chargement ..."
+        },
+        onStepChanging: function (event, currentIndex, newIndex)
+        {
+            if(newIndex < currentIndex) return true;
+            form.validate().settings.ignore = ":disabled,:hidden";
+
+            var isValid = form.valid();
+            if(isValid) {
+                var $currentTab = $("#"+$(event.target).attr("id")+"-p-"+currentIndex);
+                $("input", $currentTab).each(function(){
+                    var inputId = $(this).attr("id");
+                    $("span#recapitulatif-"+inputId).html($(this).val());
+                });
+            }
+            return isValid;
+
+
+        },
+        onFinishing: function (event, currentIndex)
+        {
+            form.validate().settings.ignore = ":disabled";
+            return form.valid();
+        },
+        onFinished: function (event, currentIndex)
+        {
+            return form.submit();
+        }
+    });
 });
-form.children("div").steps({
-    headerTag: "h3",
-    bodyTag: "section",
-    transitionEffect: "slideLeft",
-    enableFinishButton: true,
-    labels: {
-        cancel: 'Annuler',
-        current: '',
-        finish: "C'est parti",
-        next: "Suivant",
-        previous: "Précédent",
-        loading: "Chargement ..."
-    },
-    onStepChanging: function (event, currentIndex, newIndex)
-    {
-        form.validate().settings.ignore = ":disabled,:hidden";
-
-        var $currentTab = $("#"+$(event.target).attr("id")+"-p-"+currentIndex);
-        $("input", $currentTab).each(function(){
-            var inputId = $(this).attr("id");
-            $("span#recapitulatif-"+inputId).html($(this).val());
-        });
-        return form.valid();
-
-
-    },
-    onFinishing: function (event, currentIndex)
-    {
-        form.validate().settings.ignore = ":disabled";
-        return form.valid();
-    },
-    onFinished: function (event, currentIndex)
-    {
-       return form.submit();
-    }
-});
-
-
