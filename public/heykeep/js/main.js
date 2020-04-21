3869714baac0cc93c7030419fe15378c47b7606a
romain-petit39 <45284892+romain-petit39@users.noreply.github.com>
2020-04-21 12:25:11 +0200

    modal display + messagerie

--- public/heykeep/js/main.js
+++ public/heykeep/js/main.js
@@ -4,26 +4,26 @@
 /*===========[ notification ]=========================*/
 
 function showModal(message, type, titre) {
-    const toast = $(".toast");
-    toast.removeClass(" btn-success btn-danger btn-warning");
+    const $toast = $(".toast");
+    $toast.removeClass(" btn-success btn-danger btn-warning");
     
   if (type === "success") {
-    toast.addClass(" btn-success");
-    $("#notifTitle").append(titre);
-    $("#notifContent").append(message);
-    toast.toast({delay: 7000}).toast("show");
+    $toast.addClass(" btn-success");
+    $("#notifTitle").html(titre);
+    $("#notifContent").html(message);
+    $toast.toast({delay: 7000}).toast("show");
     
   } else if (type === "error") {
-    toast.addClass("btn-danger");
-    $("#notifTitle").append(titre);
-    $("#notifContent").append(message);
-    $(".toast").toast("show");
+    $toast.addClass("btn-danger");
+    $("#notifTitle").html(titre);
+    $("#notifContent").html(message);
+    $toast.toast("show");
 
   } else if (type === "warning") {
-    toast.addClass("btn-warning");
-    $("#notifTitle").append(titre);
-    $("#notifContent").append(message);
-    toast.toast({delay: 7000}).toast("show");
+    $toast.addClass("btn-warning");
+    $("#notifTitle").html(titre);
+    $("#notifContent").html(message);
+    $toast.toast({delay: 7000}).toast("show");
  
   }
 }
