02310a679c9269abdf9d1d94bc076f002bb8fe26
stephanie.debotton <stephanie.debotton@adenia.fr>
2020-04-21 14:35:02 +0200

    Ajout d'une gestion d'avatar lors de la création de profile

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -13,7 +13,7 @@ use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
 use DateTime;
 
-class RegistrationController extends AbstractController
+class RegistrationController extends BaseController
 {
     /**
      * @Route("/register", name="app_register")
@@ -23,10 +23,6 @@ class RegistrationController extends AbstractController
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
-//        dump('en cours');
-//        dump($form->isSubmitted());
-//        dump($form->isValid());
-//exit;
         if ($form->isSubmitted() && $form->isValid()) {
 
             // encode the plain password
@@ -39,16 +35,24 @@ class RegistrationController extends AbstractController
             $role = [];
             $dateNow = new DateTime();
             $isActif = true;
-            $file = $form['avatar']->getData();
-            $targetDirectory = $this->getParameter('kernel.project_dir').'/public/user/profile/'.$user->getId();
-            $file->move($targetDirectory, "avatar.".$file->guessExtension());
             $user->setRoles($role);
             $user->setCreatedDate($dateNow);
             $user->setIsactif($isActif);
 
+            $file = $form['avatar']->getData(); // Récupération du fichier pour l'avatar
+            $user->setAvatar("avatar.".$file->guessExtension()); // Affectation d'un nom standard au fichier d'avatar
+
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($user);
             $entityManager->flush();
+
+            $targetDirectory = $this->getParameter('kernel.project_dir').'/public/user/profile/'.$user->getId(); // Création du chemin vers le futur lieu de stockage de l'avatar
+            if(!is_dir($targetDirectory)) { // On test si le dossier existe déjà (ici c'est impossible car on utilise l'id du nouvel utilisateur)
+                mkdir($targetDirectory); // On créé le dossier
+                chmod($targetDirectory, 0777); //On met des droits en lecture, modification et exécution pour tous le monde => pas bien !!!
+            }
+            $file->move($targetDirectory, "avatar.".$file->guessExtension()); // On envoie le fichier sur le serveur
+            // Tindin !
 
             // do anything else you need here, like send an email
             return $guardHandler->authenticateUserAndHandleSuccess(
@@ -57,6 +61,8 @@ class RegistrationController extends AbstractController
                 $authenticator,
                 'main' // firewall name in security.yaml
             );
+        } elseif ($form->isSubmitted() && !$form->isValid()) {
+            return $this->neweditSubmittedGlobal($form);
         }
 
         return $this->render('registration/register.html.twig', [
