6738d6ec59f0b3a1576acbb632864f62cd923c87
joubert ludovic <joubertludovic@MacBook-Pro.home>
2020-04-21 15:34:13 +0200

    modif

--- public/heykeep/js/modifPassword.js
+++ public/heykeep/js/modifPassword.js
@@ -1,7 +1,21 @@
 $(document).ready(function(){     
-    //Dès qu'on clique sur #b1, on applique show()  à la page
     $("#b1").click(function(){
-        $("form1").show();
+        $("#form1").show();
+        $("#b1").hide();
     });
-    
-}); 
+    $("#b2").click(function(){
+        $("#form1").hide();
+        $("#b1").show();
+    });
+    const form = $("#form1");
+    form.validate({
+      errorPlacement: function errorPlacement(error, element) {
+        element.before(error);
+      },
+      rules: {
+        passwordConfirmation: {
+          equalTo: "#inputPassword",
+        },
+      },
+    });
+});
