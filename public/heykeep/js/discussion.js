e306ba8ae8a3fd9aa4362273816ac6e3e904e10c
romain-petit39 <45284892+romain-petit39@users.noreply.github.com>
2020-04-21 16:34:23 +0200

    spinner

--- public/heykeep/js/discussion.js
+++ public/heykeep/js/discussion.js
@@ -1,17 +1,22 @@
+
+showSpinner()
 $.ajax({
     method: "GET",
     url: 'discussion',
     success: function(data) {
         $('#discussion_list').html(data);
+        hideSpinner();
     }
 })
 function callAjaxMasterDetail(path, target, parent){
+    showSpinner();
     $.ajax({
         method: "GET",
         url: path,
         success: function(data){
            $(parent).fadeOut();
            $(target).html(data).fadeIn();
+           hideSpinner();
         }
     });
 }
