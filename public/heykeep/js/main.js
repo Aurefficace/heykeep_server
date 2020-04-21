3e13959ea6724939635c3970cbf38065b8d72cfd
romain-petit39 <45284892+romain-petit39@users.noreply.github.com>
2020-04-21 09:18:17 +0200

    modal

--- public/heykeep/js/main.js
+++ public/heykeep/js/main.js
@@ -3,55 +3,22 @@
     "use strict";
 
     
-    /*==================================================================
-    [ Validate ]*/
-    var input = $('.validate-input .input100');
 
-    $('.validate-form').on('submit',function(){
-        var check = true;
+     /*===========[ notification ]=========================*/
 
-        for(var i=0; i<input.length; i++) {
-            if(validate(input[i]) == false){
-                showValidate(input[i]);
-                check=false;
-            }
+     function showModal(message, type, titre) {
+        if (type === success) {
+            $(".toast").addClass("alert alert-success");
+            $("#notifTitle").append(titre);
+            $("#notifContent").append(message);
+            $('.toast').toast('show');
         }
-
-        return check;
-    });
-
-
-    $('.validate-form .input100').each(function(){
-        $(this).focus(function(){
-           hideValidate(this);
-        });
-    });
-
-    function validate (input) {
-        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-                return false;
-            }
+        else if (type === error) {
+            $(".toast").addClass("alert alert-danger");
+            $("#notifTitle").append(titre);
+            $("#notifContent").append(message);
+            $('.toast').toast('show');
         }
-        else {
-            if($(input).val().trim() == ''){
-                return false;
-            }
-        }
-    }
-
-    function showValidate(input) {
-        var thisAlert = $(input).parent();
-
-        $(thisAlert).addClass('alert-validate');
-    }
-
-    function hideValidate(input) {
-        var thisAlert = $(input).parent();
-
-        $(thisAlert).removeClass('alert-validate');
-    }
-    
-    
+     }
 
 })(jQuery);
