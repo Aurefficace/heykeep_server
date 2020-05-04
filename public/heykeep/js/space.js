7f67332c0cba52dbef87f51d6ee9fa197d28702d
stephanie.debotton <stephanie.debotton@adenia.fr>
2020-05-04 15:29:05 +0200

    uniformisation JS space/main

--- public/heykeep/js/space.js
+++ public/heykeep/js/space.js
@@ -1,20 +1,3 @@
-// function callAjaxMasterDetail(path, target, parent){
-//     $.ajax({
-//         method: "GET",
-//         url: path,
-//         success: function(data){
-//            $(parent).fadeOut();
-//            $(target).html(data).fadeIn();
-//         }
-//     });
-// }
-
-function displayFadeIn(target, parentone, parenttwo) {
-    $(target).fadeIn();
-    $(parentone).fadeOut();
-    $(parenttwo).fadeOut();
-}
-
 //prévisualisation image espace
 function readURL(input) { // TODO mat à mutualiser car c'est utilisé ailleurs
     if (input.files && input.files[0]) {
@@ -29,7 +12,6 @@ function readURL(input) { // TODO mat à mutualiser car c'est utilisé ailleurs
 }
 
 // Essai d'autocompletion, non fonctionnelle pour l'instant
-
 // function spaceFormCallback(){
 //     console.log("spaceFormCallback");
 //
