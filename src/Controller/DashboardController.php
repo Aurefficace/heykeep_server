a1bf25db1a3ffa04fae33d67d6f48f0f5a97e857
aurel <Freud63530!>
2020-05-04 15:10:43 +0200

    Rajout de l'affichage dynamique des espaces du dashboard

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -6,14 +6,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use App\Repository\SpaceRepository;
 
 class DashboardController extends AbstractController
 {
     /**
      * @Route("/", name="dashboard")
      */
-    public function dashboardAction()
+    public function dashboardAction(SpaceRepository $spaceRepository) : Response
     {
-        return $this->render('dashboard.html.twig');
+        return $this->render('dashboard.html.twig',
+            [
+                'spaces' => $spaceRepository->findAll(),
+
+            ]);
     }
 }
